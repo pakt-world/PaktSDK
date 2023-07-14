2d4f3a2948d77c305c54c8a80a6de83a5d48c0cc
Jendorski Labs <jendorskilabs@gmail.com>
2023-07-14 11:58:01 +0300

    - updated the ICollection with the attachment and the IUser for the creator, owner

--- examples/src/collection.ts
+++ examples/src/collection.ts
@@ -1,5 +1,4 @@
-
-import { CreateCollectionDto } from "../../../src/services/collection/collection.dto";
+import { CreateCollectionDto, CreateManyCollectionDto } from "../../src/services/collection/collection.dto";
 import PaktSDKInit from "./helper";
 
 // how to create a new collection
@@ -11,15 +10,45 @@ const CreateCollection = async () => {
       name: "New collection title",
       description: "I send a New Post to Jacob",
       isPrivate: false,
-      type: "job"
+      type: "job",
     };
-    const loginData = await sdk.collection.create(collectionPayload);
-    console.log(loginData);
+    const collection = await sdk.collection.create(collectionPayload);
+    console.log({ collection });
   } catch (error) {
     // handle error response here....
     console.log(error);
   }
-}
+};
+
+const createCollections = async () => {
+  try {
+    const sdk = await PaktSDKInit();
+    const collectionList: CreateCollectionDto[] = [
+      {
+        name: "New collection title",
+        description: "I send a New Post to Jacob",
+        isPrivate: false,
+        type: "connection",
+      },
+      {
+        name: "Collection Configuration",
+        description: "Configure your collections with different types, mark the type in creating the collection",
+        isPrivate: false,
+        type: "connection",
+      },
+    ];
+    const manyCollections: CreateManyCollectionDto = {
+      type: "connection",
+      parent: "job",
+      collections: collectionList,
+    };
+    const collections = await sdk.collection.createMany(manyCollections);
+    console.log({ collections });
+  } catch (error) {
+    // handle error response here....
+    console.log(error);
+  }
+};
 
 // create new collection type
 // const CreateCollectionType = async () => {
@@ -40,7 +69,4 @@ const CreateCollection = async () => {
 //   }
 // }
 
-
-export {
-  CreateCollection
-}
+export { CreateCollection, createCollections };
