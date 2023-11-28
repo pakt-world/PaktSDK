75b6fc9c84b166cb5d3415d47b73fbeea116fae4
Jendorski Labs <jendorskilabs@gmail.com>
2023-11-28 11:55:29 +0300

    Setup the Data Transfer Object (Dto) to ensure the right payload and responses are being gotten. - Develop the method to create a payment for a collection, thereby generating the escrow wallet address - Develop the method to validate a payment for a collection - Develop the method to release payment.

---
+++ src/services/payment/payment.dto.ts
@@ -0,0 +1,49 @@
+import { ResponseDto } from "../../utils";
+
+export type IPaymentCoins = "usdc" | "avax";
+
+export enum IPaymentStatusEnum {
+  PENDING = "pending",
+  ONGOING = "ongoing",
+  COMPLETED = "completed",
+  WAITING = "waiting",
+  CANCELLED = "cancelled",
+  DELETED = "deleted",
+}
+
+export type IPaymentStatusType = "pending" | "ongoing" | "completed" | "waiting" | "cancelled" | "deleted";
+
+export interface ICreatePaymentDto {
+  coin: IPaymentCoins;
+  collection: string;
+}
+
+export interface IPaymentDataDto {
+  coin: string;
+  address: string;
+  collectionAmount: number;
+  collectionAmountCoin: string;
+  expectedFee: string;
+  amountToPay: string;
+  usdFee: string;
+  usdAmount: string;
+  feePercentage: number;
+  rate: number;
+  chainId: string;
+}
+
+export interface IValidatePaymentDto {
+  collection: string;
+  status?: IPaymentStatusType;
+}
+
+export interface IReleasePaymentDto {
+  collection: string;
+  amount: number;
+}
+
+export interface PaymentModuleType {
+  create(authToken: string, payload: ICreatePaymentDto): Promise<ResponseDto<IPaymentDataDto>>;
+  validate(authToken: string, payload: IValidatePaymentDto): Promise<ResponseDto<{}>>;
+  release(authToken: string, payload: IReleasePaymentDto): Promise<ResponseDto<{}>>;
+}
