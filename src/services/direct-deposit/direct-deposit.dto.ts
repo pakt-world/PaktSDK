d5bab3f5d11b60d4e9c8a56aa8d277879db872ca
Jendorski <gabjide@gmail.com>
2025-08-22 14:15:55 +0000

    fix(direct-deposit): collection type defaults to payments

--- src/services/direct-deposit/direct-deposit.dto.ts
+++ src/services/direct-deposit/direct-deposit.dto.ts
@@ -3,7 +3,7 @@ import { IUser } from "../auth";
 import { ICollectionDto } from "../collection";
 
 export interface ICreateDirectDepositPayload {
-  collectionType: string;
+  collectionType: "payments";
   amount: number;
   coin: string;
   name: string;
